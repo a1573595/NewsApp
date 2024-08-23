245feaaf5aea5a4046dd1c591b9793eed4a8cfdb
a1573595 <a1573595@gmail.com>
2024-08-23 14:02:18 +0800

    Add double check exit

--- app/src/main/java/com/a1573595/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/a1573595/newsapp/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.a1573595.newsapp.ui.component.DoubleBackPress
 import com.a1573595.newsapp.ui.navigation.NavGraph
 import com.a1573595.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsAppTheme {
+                DoubleBackPress()
                 NavGraph()
             }
         }
